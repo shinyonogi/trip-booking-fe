437adaf0cdbe6384dd862a33ea5e7820f1d9e2e1
Shintaro Onogi <shinyonogi@outlook.de>
2023-11-06 01:01:59 +0100

    Added: Button Color in the header section changes with scroll

--- src/components/buttons/ButtonPrimary/index.tsx
+++ src/components/buttons/ButtonPrimary/index.tsx
@@ -10,12 +10,12 @@ type ButtonPrimaryProps = {
 const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({ label, onClick }) => {
     return (
         <motion.div
-            className='px-0.5 py-0.5 bg-black'
+            className='button-primary__wrapper px-0.5 py-0.5 bg-black'
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
             transition={{ type: 'spring', stiffness: 400, damping: 30 }}
         >
-            <button className='py-1.5 px-10 w-[calc(100%-1px)] h-[calc(100%-1px)] bg-black text-white border border-white font-normal text-lg hover:cursor-pointer focus:outline-none focus:ring' type='button' onClick={onClick}>
+            <button className='button-primary  py-1.5 px-10 w-[calc(100%-1px)] h-[calc(100%-1px)] bg-black text-white border border-white font-normal text-lg hover:cursor-pointer focus:outline-none focus:ring' type='button' onClick={onClick}>
                 <p className='button--text'>{label}</p>
             </button>
         </motion.div>
