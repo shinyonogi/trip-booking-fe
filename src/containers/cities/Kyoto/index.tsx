884c61c91ff6f1c28c771d81023ba93a4605ecff
Shintaro Onogi <shinyonogi@outlook.de>
2023-11-15 13:19:06 +0100

    Added: BigHeader component supports props where the routing can be defined

--- src/containers/cities/Kyoto/index.tsx
+++ src/containers/cities/Kyoto/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./LandingPage";
+import MajorCoursePage from "./MajorCoursePage";
 
 const Kyoto = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/majorcourse" element={<MajorCoursePage />} />
             </Routes>
         </>
     );
